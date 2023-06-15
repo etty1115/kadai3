87e4551a550fbcedc7f1992f3dc40db4b31a744a
etty1115 <miyaty1115@gmail.com>
2023-06-16 08:21:49 +0900

    お気に入り機能を実装

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+// script.js
 const form = document.getElementById('search-form');
 const previousResults = [];
+const favoriteBooks = [];
 
 form.addEventListener('submit', e => {
   e.preventDefault();
@@ -17,16 +19,16 @@ function getKeywordByExperience(experience) {
   let keyword = '';
 
   switch (experience) {
-    case '営業経験なし':
+    case '営業経験':
       keyword = '営業向け書籍';
       break;
-    case '人事労務経験なし':
+    case '人事労務経験':
       keyword = '初心者向けの人事労務関連図書';
       break;
-    case 'カスタマーサクセスなし':
+    case 'カスタマーサクセス経験':
       keyword = 'カスタマーサクセス関連書籍';
       break;
-    case 'saas経験なし':
+    case 'SaaS経験':
       keyword = 'SaaSのビジネスモデルが理解できる書籍';
       break;
     default:
@@ -86,101 +88,81 @@ async function searchBooks(keyword, experience) {
     const newWindow = window.open('', '_blank');
     newWindow.document.write(resultsDiv.outerHTML);
     newWindow.document.close();
- 
   } catch (error) {
     console.error(error);
     resultsDiv.innerHTML = '<p>検索結果を取得できませんでした。</p>';
   }
-  
+
   document.body.appendChild(resultsDiv);
+}
+
+function createBookItem(title, authors, thumbnail) {
+  const bookItem = document.createElement('li');
+
+  const imageElement = document.createElement('img');
+  imageElement.src = thumbnail;
+  imageElement.alt = title;
+  bookItem.appendChild(imageElement);
+
+  const titleElement = document.createElement('h3');
+  titleElement.textContent = title;
+  bookItem.appendChild(titleElement);
+
+  const authorElement = document.createElement('p');
+  authorElement.textContent = `著者: ${authors.join(', ')}`;
+  bookItem.appendChild(authorElement);
+
+  const favoriteButton = document.createElement('button');
+  favoriteButton.textContent = 'お気に入り';
+  favoriteButton.classList.add('favorite-button');
+  favoriteButton.addEventListener('click', () => {
+    toggleFavorite(favoriteButton, title);
+  });
+  bookItem.appendChild(favoriteButton);
+
+  return bookItem;
+}
+
+function toggleFavorite(button, title) {
+  const index = favoriteBooks.findIndex(book => book.title === title);
+
+  if (index > -1) {
+    // お気に入りが既に存在する場合は削除
+    favoriteBooks.splice(index, 1);
+    button.textContent = 'お気に入り';
+    button.classList.remove('favorited');
+  } else {
+    // お気に入りが存在しない場合は追加
+    favoriteBooks.push({ title });
+    button.textContent = 'お気に入り解除';
+    button.classList.add('favorited');
   }
-  
-  function createBookItem(title, authors, thumbnail, amazonLink) {
+
+  // お気に入り書籍の表示を更新
+  renderFavoriteBooks();
+}
+
+function renderFavoriteBooks() {
+  const favoriteBooksDiv = document.getElementById('favorite-books');
+  favoriteBooksDiv.innerHTML = '';
+
+  if (favoriteBooks.length === 0) {
+    favoriteBooksDiv.textContent = 'お気に入りがありません。';
+    return;
+  }
+
+  const heading = document.createElement('h2');
+  heading.textContent = 'お気に入り書籍';
+  favoriteBooksDiv.appendChild(heading);
+
+  const bookList = document.createElement('ul');
+
+  favoriteBooks.forEach(book => {
     const bookItem = document.createElement('li');
-  
-    const imageElement = document.createElement('img');
-    imageElement.src = thumbnail;
-    imageElement.alt = title;
-    bookItem.appendChild(imageElement);
-  
-    const titleElement = document.createElement('h3');
-    titleElement.textContent = title;
-    bookItem.appendChild(titleElement);
-  
-    const authorElement = document.createElement('p');
-    authorElement.textContent = `著者: ${authors.join(', ')}`;
-    bookItem.appendChild(authorElement);
-  
-    if (amazonLink) {
-      const amazonButton = document.createElement('a');
-      amazonButton.href = amazonLink;
-      amazonButton.textContent = 'Amazonで購入';
-      amazonButton.classList.add('amazon-button');
-      bookItem.appendChild(amazonButton);
-    }
-  
-    return bookItem;
-  }
-  
-  async function searchBooks(keyword, experience) {
-    const resultsDiv = document.createElement('div');
-    resultsDiv.classList.add('search-results');
-  
-    const heading = document.createElement('h2');
-    heading.textContent = `${experience} のおすすめ書籍`;
-    resultsDiv.appendChild(heading);
-  
-    const paragraph = document.createElement('p');
-    paragraph.textContent = `検索結果が表示される部分です。${experience} に関連する ${keyword} が表示されます。`;
-    resultsDiv.appendChild(paragraph);
-  
-    try {
-      const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(keyword)}&maxResults=5`);
-      const data = await response.json();
-  
-      const bookList = document.createElement('ul');
-      const existingTitles = []; // 重複チェックのための配列
-  
-      if (data && data.items) {
-        data.items.forEach(item => {
-          const volumeInfo = item.volumeInfo;
-          const title = volumeInfo.title;
-          const authors = volumeInfo.authors;
-          const imageLinks = volumeInfo.imageLinks;
-          const thumbnail = imageLinks ? imageLinks.thumbnail : 'noimage.png';
-          const amazonLink = volumeInfo.previewLink; // Amazonページへのリンク
-  
-          // タイトルの重複チェック
-          if (!existingTitles.includes(title)) {
-            existingTitles.push(title);
-            const bookItem = createBookItem(title, authors, thumbnail, amazonLink);
-            bookList.appendChild(bookItem);
-          }
-        });
-      }
-  
-      resultsDiv.appendChild(bookList);
-  
-      // 直前の検索結果を非表示にする
-      const previousResult = previousResults.find(result => result.experience === experience);
-      if (previousResult) {
-        previousResult.element.style.display = 'none';
-      }
-  
-      // 現在の検索結果を保存
-      previousResults.push({ experience, element: resultsDiv });
-  
-      // 新しいウェブサイトタブで検索結果を表示
-      const newWindow = window.open('', '_blank');
-      newWindow.document.write(resultsDiv.outerHTML);
-      newWindow.document.close();
-    } catch (error) {
-      console.error(error);
-      resultsDiv.innerHTML = '<p>検索結果を取得できませんでした。</p>';
-    }
-    
-    document.body.appendChild(resultsDiv);
-  }
-  
-  //
-  
+    bookItem.textContent = book.title;
+    bookList.appendChild(bookItem);
+  });
+
+  favoriteBooksDiv.appendChild(bookList);
+}
+
